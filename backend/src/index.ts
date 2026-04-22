8277dc2a9c1b8ed2334c84b298f349ba4cf922a3
Akash Singh Rautela <akashrautelacms@gmail.com>
2026-04-23 02:53:09 +0530

    host change

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -41,6 +41,6 @@ app.get('/', (req: Request, res: Response) => {
 
 const PORT = process.env.PORT || 5000;
 
-app.listen(PORT, () => {
+app.listen(Number(PORT), '0.0.0.0', () => {
   console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`);
 });
