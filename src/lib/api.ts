33e20109d291c34ac9bcb8e1a60e227fd7c1c2c7
Akash Singh Rautela <akashrautelacms@gmail.com>
2026-04-23 02:40:32 +0530

    updating

--- src/lib/api.ts
+++ src/lib/api.ts
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_URL = 'https://vehicle-finder-gie7.onrender.com';
+const API_URL = 'https://vehicle-finder-gie7.onrender.com/api';
 
 const api = axios.create({
   baseURL: API_URL,
